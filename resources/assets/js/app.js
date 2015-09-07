dcfdb1838f209b30e2d83cd71da1bdb479cfc030
Tuan Vu <vu.tuan@mulodo.com>
2015-09-07 10:35:21 +0700

    Re-define layout

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,20 @@
     var app = angular.module('mulodoApp', ['ngRoute', 'angularUtils.directives.dirPagination', 'ngMessages']);
     var api = 'http://api.mulodo.dev';
 
+    app.directive('navBar', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'layout/navbar.html'
+        };
+    });
+
+    app.directive('footer', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'layout/footer.html'
+        };
+    });
+
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/user', {
@@ -49,52 +63,37 @@
     });
 
     app.controller('userCreateController', function($scope) {
-        var compareTo = function() {
-            return {
-                require: "ngModel",
-                scope: {
-                    otherModelValue: "=compareTo"
-                },
-                link: function(scope, element, attributes, ngModel) {
-                    ngModel.$validators.compareTo = function(modelValue) {
-                        return modelValue == scope.otherModelValue;
-                    };
-
-                    scope.$watch("otherModelValue", function() {
-                        ngModel.$validate();
-                    });
-                }
-            };
-        };
-        app.directive("compareTo", function() {
-            return {
-                restrict: 'A',
-                scope:true,
-                require: 'ngModel',
-                link: function (scope, elem , attrs,control) {
-                    var checker = function () {
-         
-                        //get the value of the first password
-                        var e1 = scope.$eval(attrs.ngModel); 
-         
-                        //get the value of the other password  
-                        var e2 = scope.$eval(attrs.passwordMatch);
-                        return e1 == e2;
-                    };
-                    scope.$watch(checker, function (n) {
-         
-                        //set the form control to valid if both 
-                        //passwords are the same, else invalid
-                        control.$setValidity("unique", n);
-                    });
-                }
-            };
-        });
-
         $scope.submitForm = function(isValid) {
             arlet('our form is amazing');
         };
     });
 
+    app.directive("ngMatch", ['$parse', function($parse) {
+            var directive = {
+                link: link,
+                restrict: 'A',
+                require: '?ngModel'
+            };
+            return directive;
+
+            function link(scope, elem, attrs, ctrl) {
+                if(!ctrl) return;
+                if(!attrs.ngMatch) return;
+
+                var firstPassword = $parse(attrs.ngMatch);
+
+                var validator = function(value) {
+                    var temp = firstPassword(scope),
+                    v = value === temp;
+                    ctrl.$setValidity('match', v);
+                    return value;
+                };
+            }
+            ctrl.$parseers.unshift(validator);
+            ctrl.$formatters.push(validator);
+            attrs.$observe('ngMatch', function() {
+                validator(ctrl.$viewValue);
+            });
+        }]);
 })();
 
